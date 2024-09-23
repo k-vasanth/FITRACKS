86b9c8c7be5f5abcdf6adc4ea24e3a465281ae2a
shashi2002 <dansarishashikiran2002@gmail.com>
2024-09-23 22:46:01 +0530

    third commit

--- static/base.js
+++ static/base.js
@@ -1,19 +1,22 @@
 const coll=document.getElementById('checkbox');
 coll.addEventListener('change', function(){
+    let H='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16"><path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/></svg>';
+    let W='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16"><path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857z"/><path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/></svg>';
+    let E='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/></svg>';
     if(this.checked){
         // console.log('hello');
         document.getElementById("Sidebar").id='sidebar';
         setTimeout(function(){
-            document.getElementById("nav1").innerText="Home";
-            document.getElementById("nav2").innerText="Exercises";
-            document.getElementById("nav3").innerText="Workout Routines";
+            document.getElementById("nav1").innerHTML=H+"Home";
+            document.getElementById("nav2").innerHTML=E+"Exercises";
+            document.getElementById("nav3").innerHTML=W+"Workout Routines";
         }, 620);
     }
     else{
         // console.log('bye');
         document.getElementById("sidebar").id='Sidebar';
-        document.getElementById("nav1").innerHTML="";
-        document.getElementById("nav2").innerHTML="";
-        document.getElementById("nav3").innerHTML="";
+        document.getElementById("nav1").innerHTML=H;
+        document.getElementById("nav2").innerHTML=E;
+        document.getElementById("nav3").innerHTML=W;
     }
 })
